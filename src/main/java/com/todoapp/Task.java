26b0abcd5c0a4cecbbe4e02c14d22cfabbbf1ae0
makaimark <makaimark@gmail.com>
2017-01-31 13:23:02 +0100

    Task model with functions

--- src/main/java/com/todoapp/Task.java
+++ src/main/java/com/todoapp/Task.java
@@ -1,7 +1,44 @@
 package com.todoapp;
 
-/**
- * Created by makaimark on 2017.01.31..
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+
+    public String task;
+    public String status;
+
+//    public Task(String task, String status) {
+//        this.task = task;
+//        this.status = status;
+//    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
