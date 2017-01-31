d36f078837e127a94a006a3fd339ac5fd485b152
makaimark <makaimark@gmail.com>
2017-01-31 13:25:04 +0100

    task service modell for the database communication

--- src/main/java/com/todoapp/service/TaskService.java
+++ src/main/java/com/todoapp/service/TaskService.java
@@ -1,7 +1,29 @@
-package com.todoapp;
+package com.todoapp.service;
 
-/**
- * Created by makaimark on 2017.01.31..
- */
+import com.todoapp.Task;
+import com.todoapp.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public void setTaskRepository(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Iterable<Task> listAllTask() {
+        return taskRepository.findAll();
+    }
+
+    public void addTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Integer id) {
+        taskRepository.delete(id);
+    }
 }
